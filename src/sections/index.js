bd2487e734092dbbc62217ef8c416bff9c30e101
Oyawe <oyawegoodness@gmail.com>
2023-09-09 08:49:04 -0700

    Structure setup

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Subscribe from './Subscribe'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Footer,
+}
