15aff686c0b29ed6c4cbb2b594af4919ca2ad0f0
Oyawe <oyawegoodness@gmail.com>
2023-09-10 19:09:29 -0700

    Services section completed

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,21 @@
+import { services } from "../Constants";
+import ServiceCard from "../Components/ServiceCard";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => {
+        return (
+          <ServiceCard
+            key={service.label}
+            imageURL={service.imgURL}
+            label={service.label}
+            subtext={service.subtext}
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Services;
