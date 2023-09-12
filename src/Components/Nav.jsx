3767835d1c91e15c15cdad3ec2b1d1a80f6d0c7f
Oyawe <oyawegoodness@gmail.com>
2023-09-11 17:48:19 -0700

    nav hover effects

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
               <li key={item.label}>
                 <a
                   href={item.href}
-                  className=" font-montserrat leading-normal text-lg text-slate-gray"
+                  className=" font-montserrat leading-normal text-lg text-slate-gray hover:text-[#8aa4ed] transition ease-out"
                 >
                   {item.label}
                 </a>
